7f00553c30c971ca9376f0a50f7cb59c38e3a946
Roman Inglin <roman.inglin@gmail.com>
2022-04-03 16:20:38 +0200

    - fix after discussing in class

--- src/main/java/ch/juventus/collections/MapFilter.java
+++ src/main/java/ch/juventus/collections/MapFilter.java
@@ -2,20 +2,19 @@ package ch.juventus.collections;
 
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
-public class MapFilter<K, V> {
-    public void cleanup(Map<K, V> map, V filter)
+public class MapFilter {
+    public <K, V> void cleanup(Map<K, V> map, V filter)
     {
-        map.entrySet().removeIf(entry -> entry.getValue().equals(filter));
+        map.entrySet().removeIf(entry -> !entry.getValue().equals(filter));
     }
 
-    public void cleanupIterator(Map<K, V> map, V filter)
+    public <K, V> void cleanupIterator(Map<K, V> map, V filter)
     {
         Iterator<Map.Entry<K, V>> iterator = map.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<K, V> entry = iterator.next();
-            if (entry.getValue().equals(filter)) {
+            if (!entry.getValue().equals(filter)) {
                 iterator.remove();
             }
         }
--- src/test/java/ch/juventus/collections/MapFilterTest.java
+++ src/test/java/ch/juventus/collections/MapFilterTest.java
@@ -1,49 +1,96 @@
 package ch.juventus.collections;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapFilterTest {
-    private Map<Integer, String> map;
-    private MapFilter<Integer, String> mapFilter;
-
-    @BeforeEach
-    private void setup() {
-        map = new HashMap<>();
-        map.put(1, "alpha");
-        map.put(2, "beta");
-        map.put(3, "gamma");
-        map.put(4, "delta");
-
-        mapFilter = new MapFilter<>();
-    }
 
     @Test
     void testCleanupIntegerKeyStringValue() {
         // Arrange
+        Map<Integer, String> map = new HashMap<>();
+        MapFilter mapFilter = new MapFilter();
+        setupIntegerString(map);
         String filter = "beta";
 
         // Act
         mapFilter.cleanup(map, filter);
 
         // Assert
-        assertFalse(map.containsValue(filter), "filter is still in the map");
+        assertArrayEquals(map.values().toArray(new String[3]), new String[]{filter, filter, filter},
+                "map is not filtered correctly");
     }
 
     @Test
     void testCleanupIteratorIntegerKeyStringValue() {
         // Arrange
+        Map<Integer, String> map = new HashMap<>();
+        MapFilter mapFilter = new MapFilter();
+        setupIntegerString(map);
         String filter = "beta";
 
         // Act
         mapFilter.cleanupIterator(map, filter);
 
         // Assert
-        assertFalse(map.containsValue(filter), "filter is still in the map");
+        assertArrayEquals(map.values().toArray(new String[3]), new String[]{filter, filter, filter},
+                "map is not filtered correctly");
+    }
+
+    @Test
+    void testCleanupStringKeyLongValue() {
+        // Arrange
+        Map<String, Long> map = new HashMap<>();
+        MapFilter mapFilter = new MapFilter();
+        setupStringLong(map);
+        Long filter = 200L;
+
+        // Act
+        mapFilter.cleanup(map, filter);
+
+        // Assert
+        assertArrayEquals(map.values().toArray(new Long[4]), new Long[]{filter, filter, filter, filter},
+                "map is not filtered correctly");
+    }
+
+    @Test
+    void testCleanupIteratorStringKeyLongValue() {
+        // Arrange
+        Map<String, Long> map = new HashMap<>();
+        MapFilter mapFilter = new MapFilter();
+        setupStringLong(map);
+        Long filter = 200L;
+
+        // Act
+        mapFilter.cleanupIterator(map, filter);
+
+        // Assert
+        assertArrayEquals(map.values().toArray(new Long[4]), new Long[]{filter, filter, filter, filter},
+                "map is not filtered correctly");
+    }
+
+    private void setupIntegerString(Map<Integer, String> map) {
+        map.put(1, "alpha");
+        map.put(2, "beta");
+        map.put(3, "gamma");
+        map.put(4, "beta");
+        map.put(5, "beta");
+        map.put(6, "delta");
+    }
+
+    private void setupStringLong(Map<String, Long> map) {
+        map.put("a", 100L);
+        map.put("b", 200L);
+        map.put("c", 200L);
+        map.put("d", 300L);
+        map.put("e", 200L);
+        map.put("f", 400L);
+        map.put("g", 200L);
     }
 }
